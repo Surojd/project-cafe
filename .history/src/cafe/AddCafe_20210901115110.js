802a9753bda5454b05bb820f3b38d68da04021a0
Suroj Dangol <surojsmiling@gmail.com>
2021-09-01 14:59:53 +0545

    delete auto create file by vscode

--- .history/src/cafe/AddCafe_20210901115110.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { collection, addDoc } from "firebase/firestore";
-import firestore from './firebase';
-
-export default function AddCafe() {
-
-  return (<>AddCafe</>);
-}
--- .history/src/cafe/index_20210901110513.js
+++
@@ -1,3 +0,0 @@
-
-export { default as AddCafe } from './AddCafe';
-export { default as CafeList } from './CafeList';
